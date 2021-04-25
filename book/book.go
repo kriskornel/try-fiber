647e54df603eaa0074e86e3531916c68c27d718d
kornelius kristian r <korneliuskristian@gmail.com>
2021-04-25 23:05:40 +0700

    Add database

--- book/book.go
+++ book/book.go
@@ -1,27 +1,55 @@
 package book
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/kriskornel/try-fiber/database"
+)
 
 func GetBooks(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"data": "All books",
-	})
+	db := database.DBConn
+	var books []Book
+	db.Find(&books)
+
+	return c.JSON(books)
 }
 
 func GetBook(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"message": "Ok!",
-	})
+	id := c.Params("id")
+	db := database.DBConn
+	var book Book
+	db.Find(&book, id)
+	return c.JSON(book)
 }
 
 func NewBook(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"message": "Ok!",
-	})
+	db := database.DBConn
+	var book Book
+	book.Title = "1984"
+	book.Author = "George Orwell"
+	book.Rating = 5
+	db.Create(&book)
+	return c.JSON(book)
 }
 
 func DeleteBook(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{
-		"message": "Ok!",
-	})
+	id := c.Params("id")
+	db := database.DBConn
+
+	var book Book
+	db.First(&book, id)
+	if book.Title == "" {
+		c.Status(500).Send("No Book Found with ID")
+		return
+	}
+	db.Delete(&book)
+	return c.JSON(&fiber.Map{"message": "Book Successfully deleted"})
+}
+
+type Book struct {
+	gorm.Model
+	Title  string `json:"name"`
+	Author string `json:"author"`
+	Rating int    `json:"rating"`
 }
--- server.go
+++ server.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/kriskornel/try-fiber/book"
+	"github.com/kriskornel/try-fiber/database"
 )
 
 func setupRoutes(app *fiber.App) {
@@ -12,9 +17,22 @@ func setupRoutes(app *fiber.App) {
 	app.Delete("/api/v1/book/:id", book.DeleteBook)
 }
 
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+	if err != nil {
+		panic("Failed to connect database")
+	}
+	fmt.Println("Connection Opened to Database")
+}
+
 func main() {
 	app := fiber.New()
 
+	initDatabase()
+
 	setupRoutes(app)
 	app.Listen(":3000")
+
+	defer database.DBConn.Close()
 }
