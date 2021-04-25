f7f9f1f567df2d966c66035612e26e8b603875ce
kornelius kristian r <korneliuskristian@gmail.com>
2021-04-25 22:43:14 +0700

    Change c.Send into c.JSON

--- book/book.go
+++ book/book.go
@@ -2,18 +2,26 @@ package book
 
 import "github.com/gofiber/fiber/v2"
 
-func GetBooks(c *fiber.Ctx) {
-	c.Send("All books")
+func GetBooks(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{
+		"data": "All books",
+	})
 }
 
-func GetBook(c *fiber.Ctx) {
-	c.Send("Single Book")
+func GetBook(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{
+		"message": "Ok!",
+	})
 }
 
-func NewBook(c *fiber.Ctx) {
-	c.Send("New Book")
+func NewBook(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{
+		"message": "Ok!",
+	})
 }
 
-func DeleteBook(c *fiber.Ctx) {
-	c.Send("Delete Book")
+func DeleteBook(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{
+		"message": "Ok!",
+	})
 }
--- server.go
+++ server.go
@@ -10,15 +10,11 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/api/v1/book/:id", book.GetBook)
 	app.Post("/api/v1/book", book.NewBook)
 	app.Delete("/api/v1/book/:id", book.DeleteBook)
-
-	app.Static("/static", "./public")
-
 }
 
 func main() {
 	app := fiber.New()
 
 	setupRoutes(app)
-
 	app.Listen(":3000")
 }
